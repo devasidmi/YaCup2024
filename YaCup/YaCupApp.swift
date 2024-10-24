e4da4da76efae968dcac0153266ae034380925c6
Vasilii Dmitriev <vasily.dmitriev98@gmail.com>
2024-10-24 16:21:46 +0200

    Initial Commit

---
+++ YaCup/YaCupApp.swift
@@ -0,0 +1,17 @@
+//
+//  YaCupApp.swift
+//  YaCup
+//
+//  Created by Vasiliy Dmitriev on 24.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct YaCupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
